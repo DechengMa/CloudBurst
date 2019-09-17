001a54a1c6e2a5f809185d03586574aaa82b779c
Decheng <chinadecheng@gmail.com>
2019-09-17 22:23:34 +1000

    Project setup

---
+++ CloudBurst/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  CloudBurst
+//
+//  Created by Decheng Ma on 17/9/19.
+//  Copyright © 2019 Decheng. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+    // Mark: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
